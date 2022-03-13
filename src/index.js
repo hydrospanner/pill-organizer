ae1938c4ba018c78be671a8a53fa0ac874ea4a50
John Tucker <johntuckerusa@gmail.com>
2022-03-12 19:38:17 -0600

    Make row/col for pill organizer.

--- src/index.js
+++ src/index.js
@@ -10,50 +10,51 @@ function Square(props) {
   );
 }
 
-class Board extends React.Component {
+class Organizer extends React.Component {
   renderSquare(i) {
     return (
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        key={i}
       />
     );
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+    let squares = [];
+    for (let rowIdx = 0; rowIdx < this.props.rows; rowIdx++) {
+      let row = [];
+      for (let colIdx = 0; colIdx < this.props.cols; colIdx++) {
+        // row.push(this.renderSquare(rowIdx * this.props.rows + colIdx));
+        row.push(this.renderSquare(colIdx * this.props.cols + rowIdx));
+      }
+      squares.push(
+        <div key={rowIdx} className="board-row">
+          {row}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+      );
+    }
+    return <div>{squares}</div>;
   }
 }
 
-class Game extends React.Component {
+class Session extends React.Component {
+  // Session trying to match fulfill pill instructions
   constructor(props) {
     super(props);
+    const rows = 2;
+    const cols = 7;
     this.state = {
       history: [
         {
-          squares: Array(9).fill(null),
+          squares: Array(rows * cols).fill(null),
         },
       ],
       stepNumber: 0,
       xIsNext: true,
+      rows: rows,
+      cols: cols,
     };
   }
 
@@ -89,7 +90,7 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
+      const desc = move ? "Go to move #" + move : "Go to session start";
       return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
@@ -99,17 +100,19 @@ class Game extends React.Component {
 
     let status;
     if (winner) {
-      status = "Winner: " + winner;
+      status = "Your pill have been correctly organized!";
     } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+      status = "Selected Medication: " + (this.state.xIsNext ? "X" : "O");
     }
 
     return (
       <div className="game">
         <div className="game-board">
-          <Board
+          <Organizer
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            rows={this.state.rows}
+            cols={this.state.cols}
           />
         </div>
         <div className="game-info">
@@ -121,26 +124,27 @@ class Game extends React.Component {
   }
 }
 
+class OverLord extends React.Component {
+  // Handle modifications to game config here
+  // - (row/col changes)
+  // - instruction changes
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Pill Master 3000</h1>
+        </div>
+        <Session />
+      </div>
+    );
+  }
+}
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<OverLord />, document.getElementById("root"));
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
+  // no winner yet
+  // TODO: determine if pill org instructions have been met for all percriptions
   return null;
 }
