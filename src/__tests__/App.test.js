fc2658e95b4fd201ac1fb1df413ec62ca31f1c2a
John Tucker <johntuckerusa@gmail.com>
2022-04-10 15:30:51 -0500

    unit tests

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { App } from "../App";
+
+it("renders App", () => {
+  render(<App />);
+  expect(screen.getByText("Session Configuration")).toBeInTheDocument();
+});
---
+++ src/setupTests.js
@@ -0,0 +1,6 @@
+// https://create-react-app.dev/docs/running-tests/#initializing-test-environment
+import "@testing-library/jest-dom";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
