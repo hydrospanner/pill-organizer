953943b9f47da4549309baa3ef26340dbec4d01a
John Tucker <johntuckerusa@gmail.com>
2022-04-24 14:34:41 -0500

    Tab UI. Add About page. (#6)

---
+++ src/About.js
@@ -0,0 +1,24 @@
+export function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        This app is a tool for a therapist and patient to practice organizing
+        medications using a pill organizer.
+      </p>
+      <p>
+        The therapist will setup the patient's medications, then hand off the
+        app to the paitent to drag the pills into the organizer.
+      </p>
+      <p>
+        This is an{" "}
+        <a href="https://github.com/hydrospanner/pill-organizer">
+          Open Source project
+        </a>
+        . Feel free to contribute or &nbsp;
+        <a href="https://www.buymeacoffee.com/johntucker">buy me a Coffee</a> to
+        show your support!
+      </p>
+    </div>
+  );
+}
